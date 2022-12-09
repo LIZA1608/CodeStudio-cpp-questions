2896d5c57abff3caa9ab94027bcddba740b8814d
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2022-12-09 17:04:29 +0530

    Create LCAofthreeNodes.cpp

---
+++ LCAofthreeNodes.cpp
@@ -0,0 +1,39 @@
+/*https://www.codingninjas.com/codestudio/problems/lca-of-three-nodes_794944?leftPanelTab=0*/
+
+/**********************************************************
+    Following is the Binary Tree Node class structure.
+
+    template <typename T>
+    class BinaryTreeNode {
+    public :
+	    T data;
+	    BinaryTreeNode<T> *left;
+	    BinaryTreeNode<T> *right;
+
+	    BinaryTreeNode(T data) {
+	        this -> data = data;
+	        left = NULL;
+	        right = NULL;
+	    }
+    };
+
+***********************************************************/
+
+BinaryTreeNode<int>* lcaOfThreeNodes(BinaryTreeNode<int>* root, int node1, int node2, int node3) {
+    // Write your code here.
+    if(root==NULL){
+        return NULL;
+    }
+    if(root->data ==node1 || root->data==node2|| root->data==node3){
+        return root;
+    }
+    BinaryTreeNode<int>*left=lcaOfThreeNodes(root->left,node1,node2,node3);
+    BinaryTreeNode<int>*right=lcaOfThreeNodes(root->right,node1,node2,node3);
+    if(left==NULL){
+        return right;
+    }
+    if(right==NULL){
+        return left;
+    }
+ return root;   
+}

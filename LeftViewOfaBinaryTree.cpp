14b9d2c9468f75316a31015bd7f077600577bb7a
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2022-12-12 22:26:06 +0530

    Create LeftViewOfaBinaryTree.cpp

---
+++ LeftViewOfaBinaryTree.cpp
@@ -0,0 +1,40 @@
+/*https://www.codingninjas.com/codestudio/problems/left-view-of-a-binary-tree_920519?leftPanelTab=0*/
+
+
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+void solve(TreeNode<int>*root,vector<int>&ans,int level){
+    if(root==NULL){
+        return;
+    }
+    if(level==ans.size()){
+        ans.push_back(root->data);
+    }
+    solve(root->left,ans,level+1);
+    solve(root->right,ans,level+1);
+}
+vector<int> getLeftView(TreeNode<int> *root)
+{
+    //    Write your code here
+    vector<int>ans;
+    solve(root,ans,0);
+    return ans;
+}

ee4932c6e2920d0a40c71cb2e03777d3e0dae5cd
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2022-12-25 21:55:51 +0530

    Create BST to sorted DLL.cpp

---
+++ BST to sorted DLL.cpp
@@ -0,0 +1,21 @@
+/*https://www.codingninjas.com/codestudio/problems/bst-to-sorted-dll_1263694?leftPanelTab=1*/
+
+
+void convert(TreeNode<int>* root,TreeNode<int>* &head){
+    if(root==NULL){
+        return ;
+    }
+    convert(root->right,head);
+    root->right=head;
+    if(head!=NULL){
+    head->left=root;
+    }
+    head=root;
+    convert(root->left,head);
+}
+TreeNode<int>* bstToSortedDLL(TreeNode<int> *root)
+{
+    TreeNode<int>* head=NULL;
+    convert(root,head);
+    return head;
+}
